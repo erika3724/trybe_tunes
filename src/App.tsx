69654e3d9560b76d3c50356afea393545758b0ca
Andre <andre@pop-os.localdomain>
2023-08-31 18:38:31 -0300

    Requisito 04

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import { Route, Routes } from 'react-router-dom';
 import Login from './Components/Login';
 import Search from './Components/Search';
+import Layout from './Components/layout/Index';
 
 function App() {
   return (
     <main>
       <Routes>
-        <Route path="/" element={ <Login /> } />
-        <Route path="/search" element={ <Search /> } />
+        <Route index path="/" element={ <Login /> } />
+        <Route path="/" element={ <Layout /> }>
+          <Route path="search" element={ <Search /> } />
+          <Route path="album/:id" element={ <Search /> } />
+        </Route>
       </Routes>
     </main>
   );
---
+++ src/Components/Header.tsx
@@ -0,0 +1,31 @@
+import React, { useEffect, useState } from 'react';
+import { NavLink } from 'react-router-dom';
+import { getUser } from '../services/userAPI';
+import Carregando from './Carregando';
+
+export default function Header() {
+  const [carrregando, setCarregando] = useState<boolean>(false);
+  const [nome, setNome] = useState<string>('');
+  useEffect(() => {
+    const busca = async () => {
+      setCarregando(true);
+      const a = await getUser();
+      console.log(a);
+      setNome(a.name);
+      setCarregando(false);
+    };
+    busca();
+  }, []);
+  return (
+    <header data-testid="header-component">
+      <nav>
+        <NavLink to="/search" data-testid="link-to-search"> Busca </NavLink>
+        <NavLink to="/favorites" data-testid="link-to-favorites"> Favoritos </NavLink>
+        <NavLink to="/profile" data-testid="link-to-profile"> Perfil </NavLink>
+        {
+            carrregando ? <Carregando /> : <p data-testid="header-user-name">{nome}</p>
+        }
+      </nav>
+    </header>
+  );
+}
