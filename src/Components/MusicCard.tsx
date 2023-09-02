4be0243d65aa4fe54316ba41f29c8bd3c38d0c4a
Andre <andre@pop-os.localdomain>
2023-09-02 19:36:35 -0300

    requisito 5

--- src/Components/MusicCard.tsx
+++ src/Components/MusicCard.tsx
@@ -1,5 +1,7 @@
 import { useEffect, useState } from 'react';
 import { AlbumType, SongType } from '../types';
+import checked from '../images/checked_heart.png';
+import uncheked from '../images/empty_heart.png';
 
 type Music = {
   album: [AlbumType, ...SongType[]] | any
@@ -10,12 +12,20 @@ export default function MusicCard({ album }:Music) {
   const [albums, setAlbums] = useState<[...AlbumType[]]>([]);
   useEffect(() => {
     const a = album.filter((d: any) => d.wrapperType === 'track');
-    console.log(album);
     const b = album.filter((c: any) => c.wrapperType === 'collection');
-    console.log(b);
     setAlbums(b);
     setSong(a);
   }, [album]);
+  const heart = async (a: any) => {
+    const c = album.filter((d: { trackId: any; }) => d.trackId === a.trackId);
+    if (!c[0].favorite || c[0].favorite === false) c[0].favorite = true;
+    else c[0].favorite = false;
+    const q = song.indexOf(a);
+    const t = song.filter((e: { trackId: any; }) => e.trackId !== a.trackId);
+    t.splice(q, 0, c[0]);
+    console.log(c[0].favorite);
+    setSong(t);
+  };
   return (
     <section>
       <h1 data-testid="artist-name">{albums[0]?.artistName}</h1>
@@ -36,6 +46,21 @@ export default function MusicCard({ album }:Music) {
                   <code>audio</code>
                   .
                 </audio>
+
+                <input
+                  type="checkbox"
+                  name={ b.trackName }
+                  id={ b.trackId }
+                  onChange={ () => heart(b) }
+                  hidden
+                />
+                <label
+                  htmlFor={ b.trackId }
+                  data-testid={ `checkbox-music-${b.trackId}` }
+                >
+                  <img src={ b.favorite === true ? checked : uncheked } alt="favorite" />
+                </label>
+
               </div>
             ))
         }
